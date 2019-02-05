ebb81ad907e01696ddd22aafa3d94f5b1388ea90
tienvvpowergate <tien.vuvan@powergatesoftware.com>
2019-02-05 21:51:26 +0700

    update

--- oop/person.js
+++ oop/person.js
@@ -1,22 +1,32 @@
  // Prototypal Inheritance
  
 
- const Person = function (firstName, lastName, age) {
+ const Person = function (firstName, lastName, age, likes = []) {
     this.firstName = firstName
     this.lastName = lastName
-    this.age = age
+    this.age = age;
+    this.likes = likes;
 }
 
 // here we are using a regular function and we are not using an error function
 // because inside of our methods we also have access to this which means that we can pull out 
 // the individual pieces of information that we actually need
 Person.prototype.getBio = function (){
-    return `${this.firstName} is ${this.age}.`;
+    let bio = `${this.firstName} is ${this.age}.`;
+
+    this.likes.forEach((like) =>{
+        bio += `${this.firstName} likes ${like}.`;
+    })
+    return bio;
 }
 
-Person.prototype.location = 'Thailand';
+Person.prototype.setName = function(fullName) {
+    const names = fullName.split(' ');
+    this.firstName = names[0];
+    this.lastName = names[1];
+};
 
-const me = new Person('Andrew', 'Mead', 27)
-console.log(me.location)
+const me = new Person('Andrew', 'Mead', 27, ['Teching', 'Biking'])
+me.setName('sdfsd sdfsd')
 const person2 = new Person('tien', 'vu', 40)
 console.log(person2.getBio())
