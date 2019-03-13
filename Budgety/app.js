3b650d2563a61e23df0fc3809881ef025b3d4edd
tienvvpowergate <tien.vuvan@powergatesoftware.com>
2019-03-13 23:30:48 +0700

    update

--- Budgety/app.js
+++ Budgety/app.js
@@ -1,15 +1,25 @@
 //BUDGET controller
-var budgetController = (function () {
-  var Expense = function (id, description, value) {
+var budgetController = (function() {
+  var Expense = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
   };
 
-  var Income = function (id, description, value) {
+  var Income = function(id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
+  };
+
+  var calculateTotal = function(type) {
+    var sum = 0;
+
+    data.allItems[type].forEach(function(cur) {
+      sum = sum + cur.value;
+    });
+
+    data.totals[type] = sum;
   };
 
   var data = {
@@ -20,11 +30,13 @@ var budgetController = (function () {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
 
   return {
-    addItem: function (type, des, val) {
+    addItem: function(type, des, val) {
       var newItem, ID;
 
       // create new id
@@ -45,12 +57,35 @@ var budgetController = (function () {
 
       // return the new item;
       return newItem;
+    },
+
+    calculateBudget: function() {
+      // Calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+      // Calculate the budget: income - expense
+      data.budget = data.totals.inc - data.totals.exp;
+      // Calculate the percentage of income that we spent
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+    },
+
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        percentage: data.percentage,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp
+      };
     }
   };
 })();
 
 // UI Controller
-var UIController = (function () {
+var UIController = (function() {
   var DOMstrings = {
     inputType: '.add__type',
     inputDescription: '.add__description',
@@ -61,7 +96,7 @@ var UIController = (function () {
   };
 
   return {
-    getInput: function () {
+    getInput: function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value,
         description: document.querySelector(DOMstrings.inputDescription).value,
@@ -69,7 +104,7 @@ var UIController = (function () {
       };
     },
 
-    addListItem: function (obj, type) {
+    addListItem: function(obj, type) {
       var html, newHtml, element;
       // create html string with placeholder text
       if (type === 'inc') {
@@ -89,7 +124,7 @@ var UIController = (function () {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
-    clearFields: function () {
+    clearFields: function() {
       var fields, fieldsArray;
 
       fields = document.querySelectorAll(
@@ -102,49 +137,51 @@ var UIController = (function () {
       fieldsArray = Array.prototype.slice.call(fields);
       console.log('fieldsArray', fieldsArray);
 
-      fieldsArray.forEach(function (current, index, array) {
+      fieldsArray.forEach(function(current, index, array) {
         current.value = '';
       });
 
       fieldsArray[0].focus();
     },
 
-    getDOMstrings: function () {
+    getDOMstrings: function() {
       return DOMstrings;
     }
   };
 })();
 
 // GLOBAL controller
-var controller = (function (budgetCtrl, UICtrl) {
-  var setupEventListeners = function () {
+var controller = (function(budgetCtrl, UICtrl) {
+  var setupEventListeners = function() {
     var DOM = UICtrl.getDOMstrings();
 
     document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
-    document.addEventListener('keypress', function (event) {
+    document.addEventListener('keypress', function(event) {
       if (event.keyCode === 13 || event.which === 13) {
         ctrlAddItem();
       }
     });
   };
 
-  var updateBudget = function () {
+  var updateBudget = function() {
     // 1. Calculate the budget
+    budgetController.calculateBudget();
 
     // 2. return the budget
+    var budget = budgetController.getBudget();
 
     // 3. Display the budget on the UI
-  }
+  };
 
-  var ctrlAddItem = function () {
+  var ctrlAddItem = function() {
     var input, newItem;
 
     //todo list
     // 1. get the field input date
     input = UIController.getInput();
 
-    if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+    if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
       // 2. Add the item to budget controller
       newItem = budgetController.addItem(
         input.type,
@@ -162,11 +199,10 @@ var controller = (function (budgetCtrl, UICtrl) {
       // 5. Calculate and update Budget
       updateBudget();
     }
-
   };
 
   return {
-    init: function () {
+    init: function() {
       console.log('Application has started');
       setupEventListeners();
     }
