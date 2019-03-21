d306c2d0509d4a03d2d3cac7186a26cc627930ca
Văn tiến Vũ <vantienvu@Vans-Air.net.fpt>
2019-03-22 01:16:21 +0700

    update

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,21 @@
-const p = document.querySelectorAll('p');
+// const p = document.querySelectorAll('p');
 
-p.forEach(function(element) {
-  if (element.textContent.includes('the')) {
-    element.remove();
-  }
-});
+// p.forEach(function(element) {
+//   if (element.textContent.includes('the')) {
+//     element.remove();
+//   }
+// });
+
+
+const Person = function(name) {
+  this.name = name
+};
+
+// here we are using a regular function and we are not using an error function
+// because inside of our methods we also have access to this which means that we can pull out
+// the individual pieces of information that we actually need
+Person.prototype.displayName = function() {
+  console.log(this.name);
+};
+
+var tien = new Person('tien');
